926004d5d815d3c17176bd7e2be2bf8e7be9724f
Gaurav Tiwari <gauravkrtiwari79@gmail.com>
2024-08-21 17:35:33 +0530

    fix navbar

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -9,40 +9,42 @@ import LoginDialog from './LoginDialog';
 
 const Menu = ({ user }) => {
   return (
-    <Router>
-      {user && (
-        <Navbar bg="dark" variant="dark" expand="lg" className="mb-3">
-          <Container fluid>
-            <Navbar.Brand as={Link} to="/">
-              Blog App
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link as={Link} to="/">
-                  Blogs
-                </Nav.Link>
-                <Nav.Link as={Link} to="/users">
-                  Users
-                </Nav.Link>
-              </Nav>
-              <Nav>
-                <Nav.Item>
-                  <LoginDialog />
-                </Nav.Item>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      )}
-      <Routes>
-        <Route path="/" element={user ? <BlogPage /> : <Navigate replace to="/login" />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/users/:id" element={<User />} />
-        <Route path="/blogs/:id" element={<Blog />} />
-        <Route path="/login" element={user ? <Navigate replace to="/" /> : <LoginForm />} />
-      </Routes>
-    </Router>
+    <div className="MenuClass">
+      <Router>
+        {user && (
+          <Navbar bg="dark" variant="dark" expand="lg" className="mb-3">
+            <Container fluid>
+              <Navbar.Brand as={Link} to="/">
+                Blog App
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link as={Link} to="/">
+                    Blogs
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/users">
+                    Users
+                  </Nav.Link>
+                </Nav>
+                <Nav>
+                  <Nav.Item>
+                    <LoginDialog />
+                  </Nav.Item>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        )}
+        <Routes>
+          <Route path="/" element={user ? <BlogPage /> : <Navigate replace to="/login" />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/users/:id" element={<User />} />
+          <Route path="/blogs/:id" element={<Blog />} />
+          <Route path="/login" element={user ? <Navigate replace to="/" /> : <LoginForm />} />
+        </Routes>
+      </Router>
+    </div>
   );
 };
 
